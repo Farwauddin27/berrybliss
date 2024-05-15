d6fc25df727d9def847676ce480f5893d0408a85
Farwauddin27 <farwajaseem128@gmail>
2024-05-15 14:41:32 +0500

    initial commit

---
+++ PRACTICE/customize.js
@@ -0,0 +1,78 @@
+function changeimage1(){ 
+    var img=document.getElementById('image');
+    img.src = 'light.png';
+}
+function changeimage2(){ 
+    var img=document.getElementById('image');
+    img.src = 'medium.png';
+}
+function changeimage3(){ 
+    var img=document.getElementById('image');
+    img.src = 'strong.png';
+}
+function changesize1(){ 
+    var img=document.getElementById('image1');
+    img.src = 'small.png';
+}
+function changesize2(){ 
+    var img=document.getElementById('image1');
+    img.src = 'medium (2).png';
+}
+function changesize3(){ 
+    var img=document.getElementById('image1');
+    img.src = 'large.png';
+}function changeice1(){ 
+    var img=document.getElementById('image2');
+    img.src = 'ice.png';
+}
+function changeice2(){ 
+    var img=document.getElementById('image2');
+    img.src = 'ice1.png';
+}
+function changesugar(){ 
+    var img=document.getElementById('image4');
+    img.src = 'sugar.png';
+}
+function changesugar1(){ 
+    var img=document.getElementById('image4');
+    img.src = 'sugarfree.png';
+}
+function changemilk(){ 
+    var img=document.getElementById('image5');
+    img.src = 'milk1.png';
+}
+function changemilk1(){ 
+    var img=document.getElementById('image5');
+    img.src = 'milk2.png';
+}
+
+function changesyrup1(){ 
+    var img=document.getElementById('image6');
+    img.src = 'syrup1.png';
+}
+function changesyrup2(){ 
+    var img=document.getElementById('image6');
+    img.src = 'syrup2.png';
+}
+function changesyrup3(){ 
+    var img=document.getElementById('image6');
+    img.src = 'syrup3.png';
+}
+
+function changetop1(){ 
+    var img=document.getElementById('image7');
+    img.src = 'toppin1.png';
+}
+function changetop2(){ 
+    var img=document.getElementById('image7');
+    img.src = 'toppin2.png';
+}
+function changetop3(){ 
+    var img=document.getElementById('image7');
+    img.src = 'topping3.png';
+}
+
+function myimages(){
+    var images=document.getElementById("images");
+    img.src=["light.png","medium.png","strong.png"];
+}
